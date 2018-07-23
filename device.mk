d3164753ce052ba3bd87e95123276dc82366014a
russel5 <rnuriyev82@gmail.com>
2018-07-23 21:26:05 +0500

    not needed anymore
    Change-Id: Iaecf4b36d265d360569d2843fd83cb1659d6b3c1

--- device.mk
+++ device.mk
@@ -9,7 +9,6 @@ endif
 
 ### DALVIK/HWUI
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 DEVICE_PATH := device/sony/lilac
 
